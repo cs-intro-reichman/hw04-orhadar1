bef4cf0c8aeab8415aea16a73ebcfc2a6d9ac68a
orhadar1 <or.hadar01@post.runi.ac.il>
2025-11-22 17:02:20 +0200

    arr finish?

--- ArrCharOps.java
+++ ArrCharOps.java
@@ -161,9 +161,9 @@ public class ArrCharOps {
      *         return -2 if there is an error with the input.
      */
         public static int compareTo(String str1, String str2) {
-         if (str1 == null || str2 == null) {
+         if (str1.length() == 0 || str2.length() == 0) {
         return -2;
-         }
+        }
 
         int minLength = Math.min(str1.length(), str2.length());
 
